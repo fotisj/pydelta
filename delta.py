c7ae73437eeb73ee4271c272f7b641721682c9ab
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2014-12-02 11:04:22 +0100

    Fix: Culling default value should be compatible with profig

--- delta.py
+++ delta.py
@@ -86,7 +86,7 @@ class Config():
                                                          "into the file results.csv. The mfwords are saved too.")
         self.cfg.init("stat.mfwords", 2000, comment="number of most frequent words to use " +
                                                     "in the calculation of delta. 0 for all words")
-        self.cfg.init("stat.culling", None, type=float, comment="ratio (or absolute number, if > 1) of documents a word must appear in to be retained in the corpus.")
+        self.cfg.init("stat.culling", 0, type=float, comment="ratio (or absolute number, if > 1) of documents a word must appear in to be retained in the corpus.")
         self.cfg.init("stat.delta_choice", 0, comment="Supported Algorithms: 0. CLASSIC_DELTA, "
                                                       "1. LINEAR_DELTA, 2. QUADRATIC_DELTA, 3. ROTATED_DELTA, 4. EDERS_DELTA, 5. EDERS_SIMPLE_DELTA,"
                                                       "6. EUCLEDIAN, 7. MANHATTAN, 8. COSINE")
@@ -265,7 +265,7 @@ class Corpus(pd.DataFrame):
                 threshold = ratio
             else:
                 threshold = ceil(ratio * self.columns.size)
-        elif threshold is None:
+        elif threshold is None or threshold == 0:
             return self
 
         culled = self.replace(0, float('NaN')).dropna(thresh=threshold)
