fea787e99bf0074b05842808c1e19ebfdba7493e
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2014-12-30 14:24:28 +0100

    Moved old stuff away

---
+++ delta/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+pydelta library
+---------------
+
+Stylometrics in Python
+"""
+
+__title__ = 'delta'
+__version__ = '0.2.0'
+__author__ = 'Fotis Jannidis, Thorsten Vitt'
+
