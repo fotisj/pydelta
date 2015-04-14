3b6d2a7d82d1a77b0179e05d20cf25ac32ec3fe5
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2015-04-14 11:35:12 +0200

    Cosine distance + range-scaled data.
    In this form, results are mediocre: max ARI on corpus_DE = 0.722269

--- delta.py
+++ delta.py
@@ -42,7 +42,7 @@ import profig
 const = collections.namedtuple('Constants',
                                ["CLASSIC_DELTA", "LINEAR_DELTA", "QUADRATIC_DELTA", "ROTATED_DELTA", "EDERS_DELTA",
                                 "EDERS_SIMPLE_DELTA", "EUCLIDEAN", "MANHATTAN", "COSINE", "CANBERRA", "BRAY_CURTIS",
-                                "CHEBYSHEV", "CORRELATION", "HOOVER_P1", "COSINE_DELTA", "COSINE_EDER", "COSINE_BINARY", "COSINE_UNIT"])._make(range(18))
+                                "CHEBYSHEV", "CORRELATION", "HOOVER_P1", "COSINE_DELTA", "COSINE_EDER", "COSINE_BINARY", "COSINE_UNIT", "COSINE_RANGE"])._make(range(19))
 
 def get_argparser():
     parser = argparse.ArgumentParser()
@@ -323,6 +323,14 @@ class Corpus(pd.DataFrame):
         z_scores = self.apply(lambda f: (f - self.mean(axis=1)) / self.std(axis=1))
         return Corpus(corpus=z_scores, metadata=self.metadata, z_scores=True)
 
+    def range_scaled(self):
+        """
+        Returns a new corpus scaled by range.
+        """
+        range = self.max(axis=1) - self.min(axis=1)
+        scaled = self.apply(lambda f: (f / range))
+        return Corpus(corpus=scaled, metadata=self.metadata, range_scaled=True)
+
     def eder_std(self):
         """
         Returns a copy of this corpus that is normalized using Eder's normalization.
@@ -418,6 +426,8 @@ class Delta(pd.DataFrame):
             super().__init__(self.delta_function(corpus.binarize(), ssd.cosine))
         elif delta_choice == const.COSINE_UNIT:
             super().__init__(self.delta_function(corpus.length_normalized(), ssd.cosine))
+        elif delta_choice == const.COSINE_RANGE:
+            super().__init__(self.delta_function(corpus.range_scaled(), ssd.cosine))
         elif delta_choice == const.LINEAR_DELTA:
             super().__init__(self.delta_function(corpus, self.linear_delta, diversities=corpus.diversities()))
         elif delta_choice == const.QUADRATIC_DELTA:
